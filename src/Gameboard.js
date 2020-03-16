f300a16d72fac972df0911b27b90f3f8f1eda72a
Madhav Acharya <madhav@Madhavs-MacBook-Air.local>
2020-03-16 14:33:53 +0545

    submit game button added

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -12,10 +12,12 @@ class GameBoard extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   handleChange(event){
       console.log(event.target.value);
     this.setState({word: event.target.word});
   }
+
   handleSubmit(event){
   let word = event.target[0].value;
   this.setState(previousState => ({
@@ -24,19 +26,32 @@ class GameBoard extends Component {
   event.preventDefault();
   }
   render(){
+    const {words, approvedWords} = this.state;
     return(
-    <div id="mainboard">
-      <div className = "boards">
-      <div>
-        <Mainboard />
-        <form id="input_box" onSubmit={this.handleSubmit}>
-          <input  type="text"  onChange={this.handleChange}  value={ this.state.word } placeholder="enter word"/>
-          <input  className = "addButton"type="submit" value="Add +" />
-        </form>
-        </div>
-        <Wordlist value = {this.state.approvedWords}/>
+      <div id="mainboard">
+            <div className = "boards">
+                <div>
+                    <Mainboard />
+                    <form id="input_box" onSubmit={this.handleSubmit}>
+                        <input
+                        type="text"
+                        onChange={this.handleChange}
+                        value={words}
+                        placeholder="enter word"
+                        />
+                        <input
+                        className = "addButton"
+                        type="submit"
+                        value="Add +"
+                        />
+                    </form>
+                </div>
+                <div>
+                    <Wordlist value = {approvedWords}/>
+                    <button className = "gameSubmit"> Submit Game </button>
+                </div>
+            </div>
       </div>
-    </div>
     );
   }
 }
